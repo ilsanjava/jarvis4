6720330e537ac0468be3772e4d01f181f38b26fd
ilsanjava <kqzekqz@gmail.com>
2022-11-07 14:55:29 +0700

    belajar anr gdr

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,4 +25,40 @@ class BookController extends Controller
             'data' =>$books,
         ], 200);
     }
+
+    function store(Reqquest $request)
+    {
+        $created = Book::create([
+            'title' => $request->title,
+            'author' => $request->author,
+            'publisher' => $request->publisher,
+            'rating' => $request->rating,
+            'published_date' => $request->publish_date,
+        ]);
+
+        return response()->json([
+            'massage' => 'Resource created sucsessfully',
+            'status' => 201,
+            'data' => $created,
+        ], 201);
+    }
+    public function show($id){
+       $book = Book::find($id);
+       // Jika id tidak ditemukan
+
+       if(!$book){
+        return response()->json([
+            'message' => 'Resource not found',
+            'status' => 404,
+            'date' => $book
+        ], 404); 
+       }
+
+       //return book resoruce
+       return response()->json([
+        'message' => 'Get detail resource',
+        'status' => 200,
+        'data' => $book
+       ], 200);
+    }
 }
